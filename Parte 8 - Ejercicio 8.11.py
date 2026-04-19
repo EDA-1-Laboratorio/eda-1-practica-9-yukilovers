e2b77ec80aa279e02ec60abdb0ceb8fb724e2b56
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 10:14:23 -0600

    Create Parte 8 - Ejercicio 8.11.py

---
+++ Parte 8 - Ejercicio 8.11.py
@@ -0,0 +1,17 @@
+'''
+Ejercicio 8.11 — Opción múltiple
+¿Qué imprime el siguiente código?
+'''
+
+for i in range(5):
+    if i == 3:
+        break
+    print(i, end=" ")
+
+'''
+a) 0 1 2 3
+b) 0 1 2
+c) 0 1 2 4
+d) 3
+Respuesta: b) 0 1 2
+'''
