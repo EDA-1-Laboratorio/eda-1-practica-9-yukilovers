14dbea5102c968cf17efba1bacb14b85b57e0f84
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 14:12:04 -0600

    Create Parte 5 - Ejercicio 5.8.py

---
+++ PARTE 5/Parte 5 - Ejercicio 5.8.py
@@ -0,0 +1,20 @@
+'''
+Ejercicio 5.8 — Opción múltiple
+¿Qué imprime el siguiente código?
+'''
+
+d = {"a": 1, "b": 2, "c": 3}
+d["b"] = 20
+print(d)
+
+'''
+¿Qué imprime el siguiente código?
+d = {"a": 1, "b": 2, "c": 3}
+d["b"] = 20
+print(d)
+a) {"a": 1, "b": 2, "c": 3, "b": 20}
+b) {"a": 1, "b": 20, "c": 3}
+c) Error: no se pueden modificar los valores de un diccionario
+d) {"a": 1, "c": 3, "b": 20}
+Respuesta: b) {"a": 1, "b": 20, "c": 3}
+'''
