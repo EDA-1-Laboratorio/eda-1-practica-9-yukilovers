198392cc9457f7244687e29896f943fdf836a111
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 13:39:29 -0600

    Add variables.py for Exercise 1.1
    Create a new Python file for Exercise 1.1 demonstrating variable types.

---
+++ PARTE 1/Parte 1 - Ejercicio 1.1.py
@@ -0,0 +1,26 @@
+'''
+Ejercicio 1.1 — Replica el ejemplo
+Crea un archivo llamado variables.py y escribe el siguiente código. Ejecútalo y verifica que la salida sea la esperada.
+'''
+
+# Variables numéricas
+entero = 10
+flotante = 3.14
+booleano = True
+
+# Verificar tipos
+print(type(entero))
+print(type(flotante))
+print(type(booleano))
+
+# Constante (convención)
+GRAVEDAD = 9.81
+print("La gravedad es:", GRAVEDAD)
+
+'''
+---SALIDA:
+<class 'int'>
+<class 'float'>
+<class 'bool'>
+La gravedad es: 9.81
+'''
