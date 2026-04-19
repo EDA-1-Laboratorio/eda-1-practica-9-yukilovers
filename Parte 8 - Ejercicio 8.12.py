9ce2190f794b1b85a43eab640f3141fe25147449
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 10:16:02 -0600

    Create Parte 8 - Ejercicio 8.12.py

---
+++ Parte 8 - Ejercicio 8.12.py
@@ -0,0 +1,17 @@
+'''
+Ejercicio 8.12 — Opción múltiple
+¿Qué imprime el siguiente código?
+'''
+
+for i in range(5):
+    if i == 3:
+        continue
+    print(i, end=" ")
+
+'''
+a) 0 1 2 3 4
+b) 0 1 2
+c) 0 1 2 4
+d) 3
+Respuesta: c) 0 1 2 4
+'''
