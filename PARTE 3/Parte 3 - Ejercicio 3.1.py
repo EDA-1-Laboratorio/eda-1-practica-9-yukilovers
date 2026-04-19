e511d8662a5452db7c7f5331058275537bbedc51
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 13:51:59 -0600

    Create operadores.py with basic operations and comparisons

---
+++ PARTE 3/Parte 3 - Ejercicio 3.1.py
@@ -0,0 +1,27 @@
+'''
+Ejercicio 3.1 — Replica el ejemplo
+Crea un archivo operadores.py:
+'''
+
+a = 15
+b = 4
+
+print("Suma:", a + b)         # 19
+print("Resta:", a - b)        # 11
+print("Multiplicación:", a * b) # 60
+print("División real:", a / b)  # 3.75
+print("División entera:", a // b) # 3
+print("Módulo:", a % b)        # 3
+print("Potencia:", a ** b)     # 50625
+
+# Operadores booleanos
+x = True
+y = False
+print("x and y:", x and y)    # False
+print("x or y:", x or y)      # True
+print("not x:", not x)        # False
+
+# Comparación
+print("15 > 4:", a > b)       # True
+print("15 == 4:", a == b)     # False
+print("15 != 4:", a != b)     # True
