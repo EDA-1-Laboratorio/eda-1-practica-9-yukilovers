eb4b4a6aef83b9ef6e7024dd10aff2ca725b70fb
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 14:05:30 -0600

    Add Ejercicio 4.8 for list length question
    Add exercise 4.8 on multiple choice question about list length.

---
+++ PARTE 4/Parte 4 - Ejercicio 4.8.py
@@ -0,0 +1,21 @@
+'''
+Ejercicio 4.8 — Opción múltiple
+¿Qué imprime este código?
+'''
+
+lista = [10, 20, 30]
+lista.append([40, 50])
+print(len(lista))
+
+'''
+¿Qué imprime este código?
+lista = [10, 20, 30]
+lista.append([40, 50])
+print(len(lista))
+a) 3
+b) 4
+c) 5
+d) Error
+	Respuesta: b) 4 
+
+'''
