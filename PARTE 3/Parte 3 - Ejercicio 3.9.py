921c15e8b5700ac94cef4562001662f49544b8e0
Romel Ontiveros Mota <120353737+M0TAR0@users.noreply.github.com>
2026-04-19 14:03:42 -0600

    Create Parte 3 - Ejercicio 3.9.py

---
+++ PARTE 3/Parte 3 - Ejercicio 3.9.py
@@ -0,0 +1,18 @@
+'''
+Ejercicio 3.9 — Opción múltiple
+¿Qué imprime el siguiente código?
+'''
+
+print(5 < 10 < 20)
+print(5 < 10 > 3)
+
+'''
+¿Qué imprime el siguiente código?
+print(5 < 10 < 20)
+print(5 < 10 > 3)
+a) True y True
+b) True y False
+c) True y Error
+d) Error y Error
+Respuesta: a) True y True
+'''
