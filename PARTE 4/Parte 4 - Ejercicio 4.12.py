e7689c23bb83df8f54104c73cd472993243f8d23
Diego-May <diego1531080p@gmail.com>
2026-04-19 19:22:22 -0600

    Create Parte 4 - Ejercicio 4.12.py

---
+++ PARTE 4/Parte 4 - Ejercicio 4.12.py
@@ -0,0 +1,17 @@
+'''
+¿Qué imprime el siguiente código?
+'''
+
+nums = [3, 1, 4, 1, 5]
+nums.sort()
+print(nums)
+nums.reverse()
+print(nums)
+'''
+    a) [1, 1, 3, 4, 5] y [5, 4, 3, 1, 1]
+    b) [3, 1, 4, 1, 5] y [5, 1, 4, 1, 3]
+    c) [1, 1, 3, 4, 5] y [1, 1, 3, 4, 5]
+    d) Error: no se puede ordenar una lista con duplicados
+
+Respuesta: a)
+'''
